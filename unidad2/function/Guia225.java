24f11b8b852c9ae755bcb131b5f1bca1ecacf3fb
sanSal66 <germansh21@gmail.com>
2024-09-04 16:02:54 -0300

    Version 1.2

--- unidad2/function/Guia225.java
+++ unidad2/function/Guia225.java
@@ -4,10 +4,15 @@ import static java.lang.Math.random;
 
 public class Guia225 {
     public static void main(String[] args) {
-
+        double aleatorio = enteroAleatorio(4, 10);
+        System.out.println(aleatorio);
     }
 
-    public static int enteroAleatorio(int min, int max) {
-        return min;
+    public static double enteroAleatorio(int min, int max) {
+        double random = Math.random();
+        if (min < random) {
+            if (random < max) return random;
+        }
+        return random;
     }
 }
